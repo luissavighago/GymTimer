22d3e0e26c181e63e71360941b3e7eb2c669b96f
Fernando Matheus dos Santos <fernandodvpr@gmail.com>
2021-08-12 21:48:53 -0300

    camera

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -1,37 +1,30 @@
 import React from 'react';
-import { View, Text, Image, FlatList, PermissionsAndroid } from 'react-native';
-import Button from '../../components/Button';
+import { View, Text, Image, FlatList, PermissionsAndroid, Button } from 'react-native';
 import styles from './styles'
 import dataDB from '../../dataDB';
 
 const users = dataDB.users
 const gym = dataDB.gym
 
-export default props => {
-
-    const escanear = async () => {
-        try {
-            const granted = await PermissionsAndroid.request(
-                PermissionsAndroid.PERMISSIONS.CAMERA,
-                {
-                    title: "Cool Photo App Camera Permission",
-                    message:
-                        "Cool Photo App needs access to your camera " +
-                        "so you can take awesome pictures.",
-                    buttonNeutral: "Ask Me Later",
-                    buttonNegative: "Cancel",
-                    buttonPositive: "OK"
-                }
-            );
-            if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-                console.log("Você pode usar a Câmera");
-            } else {
-                console.log("Permissão de Câmera negada");
-            }
-        } catch (err) {
-            console.warn(err);
-        }
+const escanear = async () => {
+    const granted = await PermissionsAndroid.request(
+    PermissionsAndroid.PERMISSIONS.CAMERA,
+    {
+        title: "App Permissão de Câmera",
+        message: "O App precisa de acesso à câmera.",
+        buttonNeutral: "Pergunte-me depois",
+        buttonNegative: "Cancelar",
+        buttonPositive: "OK"
     }
+    );
+    if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+        return true;
+    } else {
+        alert('Permissão de Câmera negada');
+    }
+};
+
+export default props => {
 
     return (
         <View style={styles.container}>
@@ -40,7 +33,7 @@ export default props => {
             <View style={styles.containerImage}>
                 <Image source={require('../../assets/images/qrcode.png')} style={styles.image} />
             </View>
-            <Button qrcodeScan title="Escanear QR Code" onClick={escanear}/>
+            <Button title="Escanear QR Code" onPress={escanear}/>
             { gym.length > 0 ?
                 <View style={styles.historyList}>
                     <Text style={styles.historyTitle}>Histórico</Text>
