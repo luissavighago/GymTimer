01d1d6ac65ec82fc81dd6a76123023abdc13fe3b
Fernando Matheus dos Santos <fernandodvpr@gmail.com>
2021-08-14 13:05:00 -0300

    maps funfando

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React  from "react"
-import { SafeAreaView} from "react-native"
-import { NavigationContainer } from "@react-navigation/native"
-import { createStackNavigator } from '@react-navigation/stack'
+import SafeAreaView from "react-native"
+import NavigationContainer from "@react-navigation/native"
+import createStackNavigator from '@react-navigation/stack'
 import SignIn  from './screens/SignIn/SignIn'
 import SignUp  from './screens/SignUp/SignUp'
 import Preload  from './screens/Preload/Preload'
 
 const Stack = createStackNavigator()
 
-export default props => (
+export default Props => (
     <SafeAreaView style={{flex: 1}}>
         <NavigationContainer>
             <Stack.Navigator initialRouteName='Preload' screenOptions={screenOptions}>
--- src/screens/Maps/styles.js
+++ src/screens/Maps/styles.js
@@ -2,15 +2,13 @@ import { StyleSheet } from "react-native";
 
 
 export default StyleSheet.create({
-    container: {
+    map: {
         flex: 1,
-        justifyContent: 'flex-end',
-        alignItems: 'flex-end',
-    },map: {
         position: 'absolute',
         top: 0,
         left: 0,
         right: 0,
-        bottom: 0
+        bottom: 0,
+        ...StyleSheet.absoluteFillObject,
     },
 })
